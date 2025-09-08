18a36c001400a32a5768fda37170d4d6eee14582
KVishnuVarma <raghuvarmakalidindi12345@gmail.com>
2025-09-08 23:53:21 +0530

    day2

---
+++ src/day2/loops.java
@@ -0,0 +1,27 @@
+package day2;
+
+import java.util.Scanner;
+
+public class loops {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for (int i = 0; i <= n; i++) {
+////            System.out.print(i + " ");
+//            System.out.println("hello World");
+//        }
+
+        // While loop
+        /*
+            //Syntax:
+            while(condition){
+                body
+              }
+         */
+        int n = sc.nextInt();
+        while (n<=5){
+            System.out.println(n);
+            n++;
+        }
+    }
+}

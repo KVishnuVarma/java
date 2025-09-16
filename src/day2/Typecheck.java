49070bea8c4db317b3be527200f4b74f52e04a97
KVishnuVarma <raghuvarmakalidindi12345@gmail.com>
2025-09-16 14:09:32 +0530

    learned upto fib

---
+++ src/day2/Typecheck.java
@@ -0,0 +1,49 @@
+package day2;
+
+import java.util.Scanner;
+
+public class Typecheck {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+//        String si = sc.next().trim();
+//        char s = sc.next().trim().charAt(5);
+//
+//        System.out.println(s);
+//
+//        if (s >= 'a' && s <='z'){
+//            System.out.println("lowercase");
+//        }
+//        else {
+//            System.out.println("Upper Case");
+//        }
+//        int m = sc.nextInt();
+//        int a = 0;
+//        int b = 1;
+//        int count = 2;
+//
+//        while (count <= m){
+//            int temp = b;
+//            b = b + a;
+//            a = temp;
+//            count++;
+//        }
+//        System.out.print(b);
+
+//        int m = sc.nextInt();
+//        int a = 0;
+//        int b = 1;
+//        int count = 2;
+
+//        while (count <= m){
+//            int temp = b;
+//            b = b + a;
+//            a = temp;
+//            count++;
+//        }
+//        System.out.print(b);
+
+        String s = sc.next().split();
+        char ch = s.length;
+
+    }
+}
--- src/day2/loops.java
+++ src/day2/loops.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class loops {
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+//        Scanner sc = new Scanner(System.in);
 //        int n = sc.nextInt();
 //        for (int i = 0; i <= n; i++) {
 ////            System.out.print(i + " ");
@@ -18,10 +18,18 @@ public class loops {
                 body
               }
          */
-        int n = sc.nextInt();
-        while (n<=5){
-            System.out.println(n);
-            n++;
-        }
+//        int n = sc.nextInt();
+//        while (n<=5){
+//            System.out.println(n);
+//            n++;
+//        }
+        /*
+            //do while loop
+         */
+        int i = 1;
+        do {
+            System.out.println(i);
+            i++;
+        }while (i <= 6);
     }
 }
